92476a71b64bb183a195565f5d6eb481a366a0d3
Jonas Engström <j.e.engstrom@gmail.com>
2023-06-24 12:14:05 +0200

    Added add_person.sh

---
+++ add_person.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Adds a new person to the table people in the sqlite3 database nutrition.db.
+# Usage:
+# ./add_person.sh NAME
+
+sqlite3 nutrition.db "INSERT INTO people (
+    person_name
+)
+VALUES
+(
+    '$1'
+);"
+
+sqlite3 nutrition.db -cmd ".headers on" ".mode columns" "SELECT * FROM people WHERE id = (SELECT MAX(id) FROM people);"
