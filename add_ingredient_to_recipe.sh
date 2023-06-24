4a8cd00b2e06030b2cb8ffa66f7a0f4b17515751
Jonas Engström <j.e.engstrom@gmail.com>
2023-06-24 15:32:18 +0200

    Removed devlopment comment in add_ingredient_to_recipe.sh

--- add_ingredient_to_recipe.sh
+++ add_ingredient_to_recipe.sh
@@ -86,21 +86,3 @@ WHERE
     recipe_ingredients.grams IS $grams
 LIMIT 1
 ;"
-
-# sqlite3 nutrition.db -cmd ".headers on" ".mode columns" "SELECT
-#     food_name,
-#     food_number,
-#     energy_kcal,
-#     fat_total_g,
-#     sum_saturated_fatty_acids_g,
-#     carbohydrates_available_g,
-#     sugars_total_g,
-#     protein_g,
-#     sodium_chloride_g
-# FROM
-#     VIEW_ingredients
-# WHERE
-#     food_name LIKE '%$1%'
-# ORDER BY
-#     energy_kcal,
-#     protein_g DESC;"
